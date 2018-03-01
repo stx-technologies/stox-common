09a7c24161a779f55879e755829e57b5962c8f1d
Yair Oval <yairov@gmail.com>
2018-03-01 09:12:57 +0200

    remove unused services

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-const blockchain = require('./lib/blockchain')
+// const blockchain = require('./lib/blockchain')
 const db = require('./lib/dbConnect')
 const {createService} = require('./lib/initServer')
 const schedule = require('./lib/schedule')
 const errors = require('./lib/errors')
 const http = require('./lib/http')
-const queue = require('./lib/queue')
-const {createMqConnections, RpcRouter, mq} = require('./lib/mq')
+const {mq} = require('./lib/mq')
 
 module.exports = {
   createService,
-  blockchain,
   db,
   schedule,
   errors,
   http,
-  queue,
-  createMqConnections,
-  RpcRouter,
   mq,
 }
