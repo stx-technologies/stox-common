980795fa95fa3d86a64d9b21f34f411067cb253e
Elad Heller <eladheller@gmail.com>
2018-03-05 00:05:40 +0200

    refactor: naming, jsdoc, clea createService.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-// const blockchain = require('./lib/blockchain')
-// const db = require('./lib/dbConnect')
-const {createService} = require('./lib/initServer')
+const {createService} = require('./lib/createService')
 const schedule = require('./lib/schedule')
 const errors = require('./lib/errors')
 const http = require('./lib/http')
-const {mq} = require('./lib/mq')
 
 module.exports = {
   createService,
-  // db,
   schedule,
   errors,
   http,
-  mq,
 }
