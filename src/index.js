5337a3fd346339fd79258ddf284877a939f4ff17
“razawi” <raz.kronenberg@gmail.com>
2018-03-08 12:41:04 +0200

    added errorHandle and promise from bc_wallet

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const {createService} = require('./lib/createService')
 const schedule = require('./lib/schedule')
 const errors = require('./lib/errors')
 const http = require('./lib/http')
+const logError = require('./utils/errorHandle')
+const promiseSerial = require('./utils/promise')
 
 module.exports = {
   createService,
   schedule,
   errors,
   http,
+  logError,
+  promiseSerial,
 }
