1ee5d41bc9a41fcde4f507235124a3708e39a52c
yoave11 <yoave11@yahoo.com>
2018-03-09 14:26:16 +0200

    create service by file structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const {createService} = require('./lib/createService')
+const createFromFileStructure = require('./lib/createFromFileStructure')
 const schedule = require('./lib/schedule')
 const errors = require('./lib/errors')
 const http = require('./lib/http')
@@ -10,4 +11,5 @@ module.exports = {
   errors,
   http,
   utils,
+  createFromFileStructure,
 }
